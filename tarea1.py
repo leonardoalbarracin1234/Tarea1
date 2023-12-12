638013dc2202d067d7df9f1ce61cf81215c25138
leonardoalbarracin1234 <153647683+leonardoalbarracin1234@users.noreply.github.com>
2023-12-12 11:09:54 -0500

    Add files via upload
    leonardo albarracin

---
+++ tarea1.py
@@ -0,0 +1,48 @@
+# aprender como se utiliza cada variable para cada linea de programaacion entre estos
+a=45
+b=30
+c=a+b
+print(c)
+#en el ejercicio 1 solo le declaramos valores a la variables y ejectucamos
+c=3.5
+d=5.7
+e=c-d
+print(e)
+
+dd= 8
+ee= 9
+ff = dd * ee
+print(ff)
+
+gg = 9.8
+hh = 4.2
+jj= gg / hh
+print(jj)
+#la funcion print sirve para mostrar el valor que le diste a la variable
+
+# variables  c e ff jj
+
+#Ejercicio 2
+Nombre = "leonardo"
+Nombre2 = "Gregory"
+Apellido = "Albarracin"
+Apellido2 = "Barbecho"
+
+mm = len (Nombre + Nombre2 + Apellido + Apellido2)
+print(mm)
+# le declaramos un nombre a la variable seguido de un print para que aparezca en la consola
+
+# aprender a leer las disntintas variables y su uso
+
+#ejercicio 3
+
+ll= 3
+qq =4.5
+ww = float(ll)
+ee = int(qq)
+print(ww)
+print(ee)
+# le declaramos valores a las variables y si es entero cuenta como int y si es float cuenta como punto flotante
+# y les ponemos viceversa para que agarre solo la parte entera o la decimal
+
+
